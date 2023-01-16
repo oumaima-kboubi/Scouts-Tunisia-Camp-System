37c0c5d65b077cba8f41752ac24feba98678fe29
Oumaima Kboubi <oumaima-kboubi>
2023-01-16 10:39:02 +0100

    test

--- auth-service/router.js
+++ auth-service/router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { register, login, welcome } = require("./Controllers/authController");
-const auth = require("./Middleware/auth");
+const auth = require("./Middleware/Auth");
 const cors = require("cors");
 
 const corsOptions = {
