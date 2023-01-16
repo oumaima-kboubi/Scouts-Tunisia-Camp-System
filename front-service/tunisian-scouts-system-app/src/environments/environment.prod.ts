dfe3ed931818f2f002a0cc8e4fb866d5a82bb2f3
Oumaima Kboubi <oumaima-kboubi>
2023-01-16 17:34:17 +0100

    hopefull wowza

--- front-service/tunisian-scouts-system-app/src/environments/environment.prod.ts
+++ front-service/tunisian-scouts-system-app/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // baseURIAuth: 'http://127.0.0.1:3001',
   // baseURIMemory: 'http://127.0.0.1:3002'
-  baseURIAuth: 'http://4.211.64.22:3001/auth',
-  baseURIMemory: 'http://4.211.64.22:3002/res'
+  baseURIAuth: 'http://4.211.64.22/auth',
+  baseURIMemory: 'http://4.211.64.22/res'
 };
