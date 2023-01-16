d8e8f287564c7dfcead983799b43b772152ebdd1
Oumaima Kboubi <oumaima-kboubi>
2023-01-16 16:38:18 +0100

    test ingress

--- front-service/tunisian-scouts-system-app/src/environments/environment.prod.ts
+++ front-service/tunisian-scouts-system-app/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // baseURIAuth: 'http://127.0.0.1:3001',
   // baseURIMemory: 'http://127.0.0.1:3002'
-  baseURIAuth: 'http://ms-auth-service:3001',
-  baseURIMemory: 'http://ms-reservation-service:3002'
+  baseURIAuth: 'http://ms-front.devops:3001',
+  baseURIMemory: 'http://ms-front.devops:3002'
 };
--- front-service/tunisian-scouts-system-app/src/environments/environment.ts
+++ front-service/tunisian-scouts-system-app/src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   // baseURIAuth: 'http://127.0.0.1:3001',
   // baseURIMemory: 'http://127.0.0.1:3002'
-  baseURIAuth: 'http://ms-auth-service:3001',
-  baseURIMemory: 'http://ms-reservation-service:3002'
+  baseURIAuth: 'http://ms-front.devops:3001',
+  baseURIMemory: 'http://ms-front.devops:3002'
 };
 
 /*
