3b9c4ce7377486673a7667001099ea0fe005e880
Oumaima Kboubi <oumaima-kboubi>
2023-01-16 15:10:04 +0100

    comm test

--- front-service/tunisian-scouts-system-app/src/environments/environment.ts
+++ front-service/tunisian-scouts-system-app/src/environments/environment.ts
@@ -4,8 +4,10 @@
 
 export const environment = {
   production: false,
-  baseURIAuth: 'http://127.0.0.1:3001',
-  baseURIMemory: 'http://127.0.0.1:3002'
+  // baseURIAuth: 'http://127.0.0.1:3001',
+  // baseURIMemory: 'http://127.0.0.1:3002'
+  baseURIAuth: 'http://ms-auth-service1:3001',
+  baseURIMemory: 'http://ms-reservation-service:3002'
 };
 
 /*
